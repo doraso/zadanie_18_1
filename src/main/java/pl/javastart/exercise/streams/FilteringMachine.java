0c98945bff695055dcb97433c8ca79ec45319f8b
Dorota Szwed <dorota.soroko@gmail.com>
2017-11-13 14:58:07 +0100

    zadanie 18_1

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,23 +1,46 @@
 package pl.javastart.exercise.streams;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int i=0; i<numberList.size();i++){
+            if (numberList.get(i) % 2 == 0){
+                evenNumbers.add(numberList.get(i));
+            }
+        }
+        return evenNumbers;
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        List<Integer> outLowerThan20 = new ArrayList<>();
+        for (int i=0; i<numberList.size();i++){
+            if (numberList.get(i)>20){
+                outLowerThan20.add(numberList.get(i));
+            }
+        }
+        return outLowerThan20;
     }
 
-    public List<Book> convertToBooks(List<String> titles) {
-        return null;
+    public List<Book> convertToBooks(List<String> titles)  {
+        List<Book> convertToBooks = new ArrayList<>();
+        for (String title : titles) {
+         convertToBooks.add(new Book(title));
+        }
+
+        return convertToBooks;
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+        List<Book> convertAndReturn = new ArrayList<>();
+        for (String title : titles) {
+            if (title.substring(0,3).equals("Gra"))
+            convertAndReturn.add(new Book(title));
+        }
+        return convertAndReturn;
     }
 }
--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,15 +1,32 @@
 package pl.javastart.exercise.streams;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
-    }
+        List<String> kidNames = new ArrayList<>();
+        for (int i = 0; i < ppl.size(); i++) {
+            if (ppl.get(i).getAge() < 18) {
+                kidNames.add(ppl.get(i).getName());
+            }
+        }
+            return kidNames;
+        }
+
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+        List<User> users = new ArrayList<>();
+        List<String> nicknames = new ArrayList<>(Arrays.asList("Tomek_2", "Ania_18","Konrad_44", "Janusz_52", "Sebastian_16"));
+
+        for (int i = 0; i < people.size() ; i++) {
+            users.add(new User(people.get(i).getName(), people.get(i).getAge(),nicknames.get(i)));
+        }
+        return users;
     }
-}
+    }
+
